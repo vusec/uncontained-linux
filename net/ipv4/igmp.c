5d81f25b3d044eb5313b77ecba35bb48c78bfe59
Jakob Koschel <jakobkoschel@gmail.com>
2022-05-25 14:44:35 +0200

    net: for_each_netdev_rcu whitelist

--- net/ipv4/igmp.c
+++ net/ipv4/igmp.c
@@ -2757,10 +2757,13 @@ static inline struct ip_mc_list *igmp_mc_get_first(struct seq_file *seq)
 	struct net *net = seq_file_net(seq);
 	struct ip_mc_list *im = NULL;
 	struct igmp_mc_iter_state *state = igmp_mc_seq_private(seq);
+	struct net_device *dev = NULL;
 
 	state->in_dev = NULL;
-	for_each_netdev_rcu(net, state->dev) {
+	state->dev = NULL;
+	for_each_netdev_rcu(net, dev) {
 		struct in_device *in_dev;
+		state->dev = dev;
 
 		in_dev = __in_dev_get_rcu(state->dev);
 		if (!in_dev)
@@ -2887,11 +2890,14 @@ static inline struct ip_sf_list *igmp_mcf_get_first(struct seq_file *seq)
 	struct ip_sf_list *psf = NULL;
 	struct ip_mc_list *im = NULL;
 	struct igmp_mcf_iter_state *state = igmp_mcf_seq_private(seq);
+	struct net_device *dev = NULL;
 
 	state->idev = NULL;
 	state->im = NULL;
-	for_each_netdev_rcu(net, state->dev) {
+	state->dev = NULL;
+	for_each_netdev_rcu(net, dev) {
 		struct in_device *idev;
+		state->dev = dev;
 		idev = __in_dev_get_rcu(state->dev);
 		if (unlikely(!idev))
 			continue;
