e2e23e79e1d779882fa13c2f7c90bb84475c258d
Jakob Koschel <jkl820.git@gmail.com>
2022-07-05 23:54:10 +0200

    nosanitize: tomoyo_condition

--- security/tomoyo/condition.c
+++ security/tomoyo/condition.c
@@ -8,6 +8,11 @@
 #include "common.h"
 #include <linux/slab.h>
 
+#ifndef _UNCONTAINED_COMPLEX_ALLOC_H
+#define _UNCONTAINED_COMPLEX_ALLOC_H
+static volatile unsigned long __uncontained_complex_alloc;
+#endif /*_UNCONTAINED_COMPLEX_ALLOC_H*/
+
 /* List of "struct tomoyo_condition". */
 LIST_HEAD(tomoyo_condition_list);
 
@@ -667,6 +672,11 @@ store_value:
 		+ e.argc * sizeof(struct tomoyo_argv)
 		+ e.envc * sizeof(struct tomoyo_envp);
 	entry = kzalloc(e.size, GFP_NOFS);
+	{ // not detected by coccinelle script somehow :/
+		typeof((*entry)) __uncontained_tmp0;
+		__uncontained_complex_alloc = (unsigned long)&__uncontained_tmp0;
+	}
+
 	if (!entry)
 		goto out2;
 	*entry = e;
